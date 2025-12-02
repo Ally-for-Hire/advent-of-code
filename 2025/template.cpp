2d0a94e196a98351363c2f6d80b3897bbd965bf2
merydian9 <zacyandell@gmail.com>
2025-12-02 01:16:17 -0600

    Add template for 2025 solutions with file handling and output structure

---
+++ 2025/template.cpp
@@ -0,0 +1,23 @@
+#include "aoc.h"
+
+int main(int argc, char* argv[])
+{
+    ifstream file(argv[1]);
+    if (!file.is_open()) {
+        cout << argv[1] << " Failed to Open" << endl;
+        return -1;
+    }
+
+    int part1Answer = 0, part2Answer = 0;
+    string input, line;
+    while (getline(file, line)) 
+        input += line;
+
+    // Solution goes here
+    
+    cout << "Part 1 Answer << " << part1Answer << endl;
+    cout << "Part 2 Answer << " << part2Answer << endl;
+
+    file.close();
+    return 0;
+}
